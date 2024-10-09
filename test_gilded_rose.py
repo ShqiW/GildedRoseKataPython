18bf5bbb243f9749bf5cc7c21332633149db50dc
ShqiW <shqiw@gmail.com>
2024-10-08 21:24:37 -0700

    lab5

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,35 +9,50 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
     def test_vest_item_should_decrease_after_one_day(self):
         vest = "+5 Dexterity Vest"
-        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6)]
+        items = [Item(vest, 1, 2)]
         gr = GildedRose(items)
         gr.update_quality()
-        self.assertEqual(gr.items, [Item(vest, 0, 1), Item(vest, 8, 18), Item(vest, 3, 5)])
 
+        assert gr.items[0].name == "+5 Dexterity Vest"
+        assert gr.items[0].sell_in == 0
+        assert gr.items[0].quality == 1
+
+    
+ 
+        
     def test_aged_brie_should_increase_quality(self):
         brie = "Aged Brie"
-        items = [Item(brie, 2, 0), Item(brie, 0, 10)]
+        items = [Item(brie, 2, 0)]
         gr = GildedRose(items)
         gr.update_quality()
-        self.assertEqual(gr.items, [Item(brie, 1, 1), Item(brie, -1, 12)])
+  
+        assert gr.items[0].name == "Aged Brie"
+        assert gr.items[0].sell_in == 1
+        assert gr.items[0].quality == 1
 
     def test_backstage_passes_increase_quality(self):
         passes = "Backstage passes to a TAFKAL80ETC concert"
-        items = [Item(passes, 15, 20), Item(passes, 10, 30), Item(passes, 5, 40)]
+        items = [Item(passes, 15, 20)]
         gr = GildedRose(items)
         gr.update_quality()
-        self.assertEqual(gr.items, [Item(passes, 14, 21), Item(passes, 9, 32), Item(passes, 4, 43)])
+   
+        assert gr.items[0].name == "Backstage passes to a TAFKAL80ETC concert"
+        assert gr.items[0].sell_in == 14
+        assert gr.items[0].quality == 21
 
     def test_sulfuras_should_never_decrease_in_quality(self):
         sulfuras = "Sulfuras, Hand of Ragnaros"
-        items = [Item(sulfuras, 0, 80), Item(sulfuras, -1, 80)]
+        items = [Item(sulfuras, 0, 80)]
         gr = GildedRose(items)
         gr.update_quality()
-        self.assertEqual(gr.items, [Item(sulfuras, 0, 80), Item(sulfuras, -1, 80)  ])
+
+        assert gr.items[0].name == "Sulfuras, Hand of Ragnaros"
+        assert gr.items[0].sell_in == 0
+        assert gr.items[0].quality == 80
 
 
 
